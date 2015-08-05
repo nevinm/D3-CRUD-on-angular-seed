deefe105a5143479df876d5da949042077a84f7d
Nevin Madhukar K <nevinm@qburst.com>
2015-08-05 23:39:50 +0530

    Updated with the dependencies

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('app', [
   'ngRoute',
-  'myApp.profile',
-  'myApp.view2',
-  'myApp.version'
+  'app.profile',
+  'app.view2',
+  'app.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/profile'});
--- app/view1/profile.js
+++ app/view1/profile.js
@@ -1,16 +1,15 @@
 'use strict';
 
-var app = angular.module('myApp.profile', ['ngRoute', 'ngProgress']);
+var app = angular.module('app', ['ngResource', 'ngRoute', 'ngProgress']);
 
-app.run(function($rootScope, ngProgress){
-	$rootScope.$on('$routeChangeStart', function(ev, data){
-		ngProgress.start();
-	});
-
-	$rootScrope.$on('$routeChangeSuccess', function(ev, data){
-		ngProgress.complete();
-	});
-});
+// app.run(function ($rootScope, ngProgress) {
+//     $rootScope.$on('$routeChangeStart', function (ev, data) {
+//         ngProgress.start();
+//     });
+//     $rootScope.$on('$routeChangeSuccess', function (ev, data) {
+//         ngProgress.complete();
+//     });
+// });
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/profile', {
