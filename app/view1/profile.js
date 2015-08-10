f95590844ebcd0ae3aa9a6372aa4f42cf3abba48
Nevin <nevinm@qburst.com>
2015-08-10 17:06:19 +0530

    Profile - View 1 Updated with add/edit/delete

--- app/view1/profile.js
+++ app/view1/profile.js
@@ -1,24 +1,109 @@
-'use strict';
-
-var app = angular.module('myApp.profile', ['ngRoute', 'ngProgress']);
-
-app.run(function($rootScope, ngProgress){
-	$rootScope.$on('$routeChangeStart', function(ev, data){
-		ngProgress.start();
-	});
-
-	$rootScrope.$on('$routeChangeSuccess', function(ev, data){
-		ngProgress.complete();
-	});
-});
+var app = angular.module('studyAppProfile', [
+    'ngRoute',
+    'ngProgress',
+    'door3.css',
+    'ui.bootstrap'
+]);
 
 app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/profile', {
-    templateUrl: 'view1/profile.html',
-    controller: 'profileCtrl'
-  });
+    $routeProvider.when('/profile', {
+        templateUrl: 'view1/profile.html',
+        controller: 'userCtrl',
+        css: 'view1/profile.css'
+    });
 }])
 
-app.controller('profileCtrl', [function($scope) {
+app.controller('userCtrl', function($rootScope, $scope, $modal, $http) {
+    $rootScope.userDetails = [];
+    $scope.user = {};
+    $scope.animationsEnabled = true;
 
-}]);
+    $http.get('http://inorthwind.azurewebsites.net/Service1.svc/getAllCustomers')
+        .then(function(res) {
+            $rootScope.userDetails = res.data.GetAllCustomersResult;
+        });
+
+    $scope.deleteUser = function(index) {
+        var deleteInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'view1/deleteUser.html',
+            controller: 'deleteUserCtrl',
+            size: "sm",
+            resolve: {
+                index: function() {
+                    return index;
+                }
+            }
+        });
+    }
+
+    $scope.addUser = function() {
+        var addInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'view1/addUser.html',
+            size: "sm",
+            controller: 'addUserCtrl',
+            resolve: {
+                user: function() {
+                    return $scope.user;
+                }
+            }
+        });
+
+        addInstance.result.then(function(data) {
+            $scope.user = data;
+            $rootScope.userDetails.unshift($scope.user);
+        }, function() {});
+    }
+
+    $scope.editUser = function(index, scope) {
+        var editInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'view1/addUser.html',
+            size: "sm",
+            controller: 'editUserCtrl',
+            resolve: {
+                user: function() {
+                    return scope;
+                }
+            }
+        });
+
+        // addInstance.result.then(function(data) {
+        //     $scope.user = data;
+        //     $rootScope.userDetails($scope.user);
+        // }, function() {});
+    }
+});
+
+app.controller('deleteUserCtrl', function($rootScope, $scope, $modalInstance, index) {
+    $scope.index = index;
+
+    $scope.ok = function() {
+        $scope.userDetails.splice(index, 1);
+        $modalInstance.close($scope.index);
+    };
+
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
+});
+
+app.controller('addUserCtrl', function($rootScope, $scope, $modalInstance, user) {
+    $scope.ok = function() {
+        $modalInstance.close($scope.user);
+    };
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
+});
+
+app.controller('editUserCtrl', function($rootScope, $scope, $modalInstance, user) {
+    $scope.user = user;
+    $scope.ok = function() {
+        $modalInstance.close($scope.user);
+    };
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
+});
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -1,15 +1,15 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('studyAppTest.view1 module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('studyAppTest.view1'));
 
   describe('view1 controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+      var userCtrl = $controller('userCtrl');
+      expect(userCtrl).toBeDefined();
     }));
 
   });
